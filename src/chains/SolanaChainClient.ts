e45a8fb76e6ac0afaee3fdff7c0b65fd21073694
michalrozekariane <michal.rozek@arianelabs.com>
2025-06-11 09:19:10 +0200

    feat: solana transferNativeFT implementation with hardcoded keys

--- src/chains/SolanaChainClient.ts
+++ src/chains/SolanaChainClient.ts
@@ -1,11 +1,20 @@
-import { AbstractChainClient } from "./abstract/AbstractChainClient";
-import { ChainConfig, TransactionResult } from "../types";
-import { ConfigService } from "../services/ConfigService";
+import { AbstractChainClient } from './abstract/AbstractChainClient';
+import { ChainConfig, SupportedOperation, TransactionResult } from '../types';
+import { ConfigService } from '../services/ConfigService';
+import {
+  Keypair,
+  Connection,
+  PublicKey,
+  LAMPORTS_PER_SOL,
+  Transaction,
+  SystemProgram,
+  sendAndConfirmTransaction,
+  Commitment,
+} from '@solana/web3.js';
 
 export class SolanaChainClient extends AbstractChainClient {
   constructor(chainConfig: ChainConfig, configService: ConfigService) {
     super(chainConfig, configService);
-    // create instance of wallet
   }
 
   // Native Fungible Token Operations
@@ -22,6 +31,53 @@ export class SolanaChainClient extends AbstractChainClient {
   }
 
   async transferNativeFT(): Promise<TransactionResult> {
-    throw new Error('Method not implemented.');
+    const commitment: Commitment = 'confirmed';
+    const accountPrivateKeyBytes = Uint8Array.from(
+      JSON.parse(this.credentials.privateKey!)
+    );
+
+    const sender = Keypair.fromSecretKey(accountPrivateKeyBytes);
+    const connection = new Connection(
+      'https://api.devnet.solana.com',
+      commitment
+    );
+    const to = new PublicKey('3hjqHVbLQTaaUyfn6dPKtws33xpPk7ZuigSS9TdBLBHy');
+    const amountSol = 0.01;
+
+    const transaction = new Transaction().add(
+      SystemProgram.transfer({
+        fromPubkey: sender.publicKey,
+        toPubkey: to,
+        lamports: amountSol * LAMPORTS_PER_SOL,
+      })
+    );
+
+    const transactionSignature = await sendAndConfirmTransaction(
+      connection,
+      transaction,
+      [sender]
+    );
+
+    const transactionDetails = await connection.getParsedTransaction(
+      transactionSignature,
+      {
+        maxSupportedTransactionVersion: 0,
+      }
+    );
+
+    const { fee } = transactionDetails?.meta ?? {};
+    console.log(' fee:', fee);
+
+    return {
+      chain: 'solana',
+      operation: SupportedOperation.TRANSFER_NATIVE_FT,
+      transactionHash: transactionSignature,
+      gasUsed: '',
+      gasPrice: '',
+      totalCost: '',
+      nativeCurrencySymbol: 'SOL',
+      timestamp: new Date().toISOString(),
+      status: 'success',
+    };
   }
 }
