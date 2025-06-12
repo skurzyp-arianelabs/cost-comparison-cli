44323b870e00aa98a483042931d7f28f1a0c5c81
michalrozekariane <michal.rozek@arianelabs.com>
2025-06-12 16:10:05 +0200

    feat: add NFT methods for AbstractChainClient

--- src/chains/abstract/AbstractChainClient.ts
+++ src/chains/abstract/AbstractChainClient.ts
@@ -1,8 +1,12 @@
-import { ChainConfig, SupportedOperation, TransactionResult } from "../../types";
-import { IChainClient } from "../IChainClient";
-import { ConfigService } from "../../services/ConfigService";
-import { CoinGeckoApiService } from "../../services/ApiService/CoinGeckoApiService";
-import { AbstractWalletService } from "../../services/WalletServices/AbstractWalletService";
+import {
+  ChainConfig,
+  SupportedOperation,
+  TransactionResult,
+} from '../../types';
+import { IChainClient } from '../IChainClient';
+import { ConfigService } from '../../services/ConfigService';
+import { CoinGeckoApiService } from '../../services/ApiService/CoinGeckoApiService';
+import { AbstractWalletService } from '../../services/WalletServices/AbstractWalletService';
 
 export abstract class AbstractChainClient implements IChainClient {
   protected chainConfig: ChainConfig;
@@ -10,7 +14,11 @@ export abstract class AbstractChainClient implements IChainClient {
   protected coinGeckoApiService: CoinGeckoApiService;
   protected walletService: AbstractWalletService;
 
-  constructor(chainConfig: ChainConfig, configService: ConfigService, walletService: AbstractWalletService) {
+  constructor(
+    chainConfig: ChainConfig,
+    configService: ConfigService,
+    walletService: AbstractWalletService
+  ) {
     this.chainConfig = chainConfig;
     this.configManager = configService;
     this.coinGeckoApiService = new CoinGeckoApiService();
@@ -58,6 +66,22 @@ export abstract class AbstractChainClient implements IChainClient {
     throw new Error('Method not implemented.');
   }
 
+  async createNativeNFT(): Promise<TransactionResult> {
+    throw new Error('Method not implemented.');
+  }
+
+  async associateNativeNFT(): Promise<TransactionResult> {
+    throw new Error('Method not implemented.');
+  }
+
+  async mintNativeNFT(): Promise<TransactionResult> {
+    throw new Error('Method not implemented.');
+  }
+
+  async transferNativeNFT(): Promise<TransactionResult> {
+    throw new Error('Method not implemented.');
+  }
+
   getChainInfo(): ChainConfig {
     return this.chainConfig;
   }
@@ -66,7 +90,9 @@ export abstract class AbstractChainClient implements IChainClient {
     throw new Error('Method not implemented.');
   }
 
-  async executeOperation(operation: SupportedOperation): Promise<TransactionResult> {
+  async executeOperation(
+    operation: SupportedOperation
+  ): Promise<TransactionResult> {
     try {
       switch (operation) {
         case SupportedOperation.CREATE_NATIVE_FT:
@@ -89,17 +115,29 @@ export abstract class AbstractChainClient implements IChainClient {
           return await this.mintERC20_RPC();
         case SupportedOperation.TRANSFER_ERC20_HARDHAT:
           return await this.transferERC20_RPC();
+        case SupportedOperation.CREATE_NATIVE_NFT:
+          return await this.createNativeNFT();
+        case SupportedOperation.ASSOCIATE_NATIVE_NFT:
+          return await this.associateNativeNFT();
+        case SupportedOperation.MINT_NATIVE_NFT:
+          return await this.mintNativeNFT();
+        case SupportedOperation.TRANSFER_NATIVE_NFT:
+          return await this.transferNativeNFT();
         default:
-          throw new Error(`executeOperation: Operation '${operation}' is not implemented or supported.`);
+          throw new Error(
+            `executeOperation: Operation '${operation}' is not implemented or supported.`
+          );
       }
     } catch (error: any) {
-      console.error(`Error during operation '${operation}': ${error.message || error}`);
+      console.error(
+        `Error during operation '${operation}': ${error.message || error}`
+      );
       return {
         status: 'failed',
         error: error.message || error,
         timestamp: Date.now().toLocaleString(),
         operation,
-        chain: this.chainConfig.type
+        chain: this.chainConfig.type,
       };
     }
   }
